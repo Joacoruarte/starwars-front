a2697c1614c3770f5e6b420a2d658bed322545b4
Joacoruarte <ruartejoaquin@gmail.com>
2024-04-16 09:29:42 -0300

    Refactor ContainerPlanets component in planets page

--- src/app/planets/components/ContainerPlanets.tsx
+++ src/app/planets/components/ContainerPlanets.tsx
@@ -7,7 +7,7 @@ interface PlanetsProps {
   planets: PlanetResponse;
 }
 
-export default function ContainerPlanets({ planets }: PlanetsProps) {  
+export default function ContainerPlanets({ planets }: PlanetsProps) {
   return (
     <StarWarsProvider entity={Entity.PLANETS} defaultData={planets}>
       <Planets />
